803560603118dbeff2b66cfaa3995837d96b0c62
vts-software <via.tsiamruk@gmail.com>
2025-10-19 16:26:41 +0200

    ток сецчас доделал shop/urls и подключил myshop/urls

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -6,13 +6,12 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # стандартная админка Django
-    # path('', include('shop.urls')),  # позже сюда подключим urls приложения shop
+    path('admin/', admin.site.urls),
+    path('', include('shop.urls')),  # urls приложения shop
     # path('users/', include('users.urls')),  # позже подключим urls приложения users
 ]
 
-# раздача медиа-файлов в режиме разработки
-# DEBUG=True позволяет Django обслуживать изображения локально
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
